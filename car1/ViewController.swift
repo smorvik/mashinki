fc0bd3c614c34a6b757b5434042703fbd11e9070
Гость <Guest@iMac-Mac005-6.local>
2022-04-26 12:31:39 +0900

    first commit

---
+++ car1/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  car1
+//
+//  Created by Гость on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var pcCar: UIImageView!
+    @IBOutlet weak var userCar: UIImageView!
+    @IBOutlet weak var lineFinish: UIImageView!
+    @IBOutlet weak var semaforLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var stateSemafor: Int = 1
+    var timerGame: Timer!
+    var timerPC: Timer!
+    
+    @objc func pcDrive() {
+        if stateSemafor == 2 {
+            pcCar.center.x += 10
+        }
+        if pcCar.center.x > lineFinish.center.x {
+            resultLabel.isHidden = false
+            resultLabel.text = "YOU LOSE!!!!"
+            resultLabel.textColor = .red
+            timerPC.invalidate()
+            timerGame.invalidate()
+        }
+    }
+    
+    @objc func intervalTimer() {
+        stateSemafor += 1
+        
+        if stateSemafor > 2 {
+            stateSemafor = 1
+        }
+        switch stateSemafor {
+        case 1:
+            semaforLabel.text = "STOP!!!!"
+            semaforLabel.textColor = .red
+        case 2:
+            semaforLabel.text = "DRIVE!!!!"
+            semaforLabel.textColor = .green
+        default:
+            break
+        }
+    }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+        semaforLabel.isHidden = false
+        
+        
+        timerGame =
+        Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(intervalTimer), userInfo: nil, repeats: true)
+        timerPC =
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(pcDrive), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func driveGame(_ sender: UIButton) {
+        if stateSemafor == 2 {
+            userCar.center.x += 10
+        } else if stateSemafor == 1 {
+            userCar.center.x -= 10
+        }
+        if userCar.center.x > lineFinish.center.x {
+            resultLabel.isHidden = false
+            resultLabel.text = "YOU WIN!!!!"
+            resultLabel.textColor = .blue
+            timerPC.invalidate()
+            timerGame.invalidate()
+        }
+    }
+        
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
